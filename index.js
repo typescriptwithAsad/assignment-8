5cebc75438323a163a49ce2edbf9005a16f930a0
asad <139746796+typescriptwithAsad@users.noreply.github.com>
2023-07-20 20:18:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+/* 8.	You should create four lines that look like this:
+________________________________________
+console.log(5 + 3)
+________________________________________
+Your output should simply be four lines with the number 8 appearing once on each line.
+*/
+console.log('_'.repeat(80)); // Here use Repeat method for line
+console.log(5 + 3); // Addition operator
+console.log('_'.repeat(80));
+console.log(16 - 8); // subtraction operator         
+console.log('_'.repeat(80));
+console.log(4 * 2); // multiplication operator     
+console.log('_'.repeat(80));
+console.log(16 / 2); // divition operator  
+console.log('_'.repeat(80));
